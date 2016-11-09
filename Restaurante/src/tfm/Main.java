cdca1daf268e2ab166729ac2d9563e8d8151bf55
Usuario <Usuario@PC-001>
2016-11-10 00:16:09 +0100

    add main

--- Restaurante/src/tfm/Main.java
+++ Restaurante/src/tfm/Main.java
@@ -11,52 +11,56 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Inicio");
-		
-    	ConexionSQL conexionSQL = new ConexionSQL();
-    	conexionSQL.conectar();
-    	double[][]matriz = conexionSQL.getMATRIZ();
-		Algoritmo algoritmo = new Algoritmo(matriz, conexionSQL.getFILAS(), conexionSQL.getCOLUMNAS()) ;
-		
-		
-//		double[][] matriz = matrizPrueba;
-//    	AlgoritmoObj2 algoritmo = new AlgoritmoObj2(matriz, 6, 12);
-    	System.out.println("-------------------");
+
+		ConexionSQL conexionSQL = new ConexionSQL();
+		conexionSQL.conectar();
+		double[][] matriz = conexionSQL.getMATRIZ();
+		Algoritmo algoritmo = new Algoritmo(matriz, conexionSQL.getFILAS(),
+				conexionSQL.getCOLUMNAS());
+
+		// double[][] matriz = matrizPrueba;
+		// AlgoritmoObj2 algoritmo = new AlgoritmoObj2(matriz, 6, 12);
+		System.out.println("-------------------");
 		algoritmo.crearMatrizItemsComunesEntreUsuarios();
 		algoritmo.calculoMSD();
 		algoritmo.crearListaCompatibilidadOrdenada();
-		//algoritmo.pintarListaCompatibilidadOrdenada();
-		
-		int u = Algoritmo.FILAS-1;
-		algoritmo.construirMatrizEstimacionVotos(u);//cantidad de usarios con los que se quiere estimar 
-//		System.out.println("MATRIZ DE ESTIMACION!---------------------------------");
-		//algoritmo.pintarMatrizEstimacion();
-		//System.out.println("MATRIZ DE ESTIMACION ORDENADA!---------------------------------");
+		// algoritmo.pintarListaCompatibilidadOrdenada();
+
+		int u = Algoritmo.FILAS - 1;
+		algoritmo.construirMatrizEstimacionVotos(u);// cantidad de usarios con
+													// los que se quiere estimar
+		// System.out.println("MATRIZ DE ESTIMACION!---------------------------------");
+		// algoritmo.pintarMatrizEstimacion();
+		// System.out.println("MATRIZ DE ESTIMACION ORDENADA!---------------------------------");
 		algoritmo.construirMatrizEstimacionOrdenada();
-		//algoritmo.pintarMatrizEstimacionOrdenada();
-		
+		// algoritmo.pintarMatrizEstimacionOrdenada();
+
 		menu(algoritmo, conexionSQL);
-		
+
 	}
-	
-	
 
 	private static void menu(Algoritmo algoritmo, ConexionSQL conexionSQL) {
 
-		
-		int usuario = seleccionaOpcion("Introduce un usuario n [ 0, 1, 2,..., " + Algoritmo.FILAS + " como máximo]", "Intenta introducir un número");
-		int ususariosCompatibles = seleccionaOpcion("Introduce la cantidad de usuarios compatibles que quiere ver [ " + Algoritmo.FILAS + " como máximo]", "Intenta introducir un número");
+		int usuario = seleccionaOpcion("Introduce un usuario n [ 0, 1, 2,..., "
+				+ Algoritmo.FILAS + " como máximo]",
+				"Intenta introducir un número");
+		int ususariosCompatibles = seleccionaOpcion(
+				"Introduce la cantidad de usuarios compatibles que quiere ver [ "
+						+ Algoritmo.FILAS + " como máximo]",
+				"Intenta introducir un número");
 
 		System.out.println("El Usuario " + usuario + "es compatible con:");
-		algoritmo.pintarListaCompatibilidadOrdenada(usuario, ususariosCompatibles);
+		algoritmo.pintarListaCompatibilidadOrdenada(usuario,
+				ususariosCompatibles);
 		System.out.println("Los posibles ítems que le pueden gustar son:");
-		ArrayList<PorcentajeSimilitud> aux = algoritmo.pintarMatrizEstimacionOrdenada(usuario);
+		ArrayList<PorcentajeSimilitud> aux = algoritmo
+				.pintarMatrizEstimacionOrdenada(usuario);
 		System.out.println("Los items asociados son");
 		conexionSQL.consutaItemsSegunId(aux);
-		System.out.println("Los restaurantes recomendados segun los ítems son:");
+		System.out
+				.println("Los restaurantes recomendados segun los ítems son:");
 		conexionSQL.consultaRestauranteSeunItem(aux);
 	}
-
-
 
 	private static int seleccionaOpcion(String mensaje, String mensajeError) {
 		Scanner sc = new Scanner(System.in);
@@ -66,17 +70,15 @@ public class Main {
 		do {
 			try {
 				opcion = sc.nextInt();
-				ok=true;
+				ok = true;
 			} catch (InputMismatchException e) {
-				// TODO: handle exception
 				System.out.println(mensajeError);
 				sc.next();
 			}
-			
-		} while (!ok);
-		
-		return opcion ;
-	}
 
+		} while (!ok);
+
+		return opcion;
+	}
 
 }
